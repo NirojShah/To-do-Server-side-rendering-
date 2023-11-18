cdc914b4c7f3cc325925e74aac049ef8677f1f4e
Niraj Shah <Nirajshah950@gmail.com>
2023-11-18 22:39:45 +0530

    please

--- Server/server.js
+++ Server/server.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose")
 const app = require("./app")
 const dotenv = require("dotenv")
 
-dotenv.config({
-    path:"./.env"
-})
+// dotenv.config({
+//     path:"./.env"
+// })
 
+
+MONGODB_CLOUD = `mongodb+srv://nirajshah950:92ic8oHSXr1BkwqZ@todoapplication.mq2dffn.mongodb.net/?retryWrites=true&w=majority`
+MONGODB_LOCAL= `mongodb://127.0.0.1:27017/ToDoTasks`
+PORT=5000
 
 // Connect to DB
 // mongoose.connect("mongodb://127.0.0.1:27017/ToDoTasks").then(() => {
@@ -21,7 +25,7 @@ dotenv.config({
 
 
 
-mongoose.connect(process.env.MONGODB_CLOUD).then(() => {
+mongoose.connect(MONGODB_CLOUD).then(() => {
     // console.log("Connected to DB")
 }).catch((err) => {
     console.log(err)
@@ -30,7 +34,7 @@ mongoose.connect(process.env.MONGODB_CLOUD).then(() => {
 
 let server = http.createServer(app)
 
-server.listen(process.env.PORT, (err) => {
+server.listen(PORT, (err) => {
     if (err) console.log(err)
     // console.log("SERVER RUNNING AT PORT NO : " + process.env.PORT)
 })
